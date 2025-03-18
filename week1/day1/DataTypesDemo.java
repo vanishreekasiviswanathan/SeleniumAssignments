0a2e21092d845b99903b1faf9f346d00bb0a45b2
vanishreekasiviswanathan <41100899+vanishreekasiviswanathan@users.noreply.github.com>
2025-03-18 07:36:31 +0530

    Add files via upload

---
+++ week1/day1/DataTypesDemo.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+		byte byteData = 120;
+		short shortData = 30000;
+		int intData = 1000000000;
+		long longData = 9005034876L;
+		float floatData = 1000.54545F;
+		Double doubleData = 200005.5348796;
+		char chData = 'B';
+		char chData2 = 66;
+		String strData = "Selenium";
+		System.out.println(byteData+" "+shortData+" "+intData+" "+longData+" "
+		                      +floatData+" "+doubleData+" "+chData+" "+chData2+" "+strData);
+		
+	}
+
+}
