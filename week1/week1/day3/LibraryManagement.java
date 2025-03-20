8d8acfb5a825e17e00f3c3c0b788b5b9a486f0e2
vanishreekasiviswanathan <41100899+vanishreekasiviswanathan@users.noreply.github.com>
2025-03-20 09:14:40 +0530

    Add files via upload

---
+++ week1/week1/day3/LibraryManagement.java
@@ -0,0 +1,12 @@
+package week1.day3;
+
+public class LibraryManagement {
+
+	public static void main(String[] args) {
+		Library management1 = new Library();
+		management1.addBook("PSYCHOLOGY OF MONEY");
+		management1.issueBook();
+
+	}
+
+}
